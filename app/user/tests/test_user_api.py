5676a4d87cc9ad05ec3722820d888ce2286782e3
maxim.velesyuk <maxim.velesyuk@travelperk.com>
2020-09-18 15:15:10 +0200

    Add auth tokens

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -38,9 +39,31 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
-        payload = {'email': 'test@email.com', 'password': '1'}
+        payload = {'email': 'test@email.com',
+                   'password': '1', 'name': '1'}
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']).exists()
         self.assertFalse(user_exists)
+
+    def test_create_user_token(self):
+        payload = {'email': 'test@email.com', 'password': 'passwd'}
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('token', res.data)
+
+    def test_create_token_invalid(self):
+        payload = {'email': 'test@email.com', 'password': 'passwd'}
+        create_user(**payload)
+        payload['password'] = '1'
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+    def test_create_token_no_user(self):
+        payload = {'email': 'test@email.com', 'password': 'passwd'}
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
